de0d58c6681dd0f34e1caf412b67374ee389d23a
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 00:14:16 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div>
       <footer>
       <div className='h-10 flex justify-center items-center bg-blue-950 px-8  text-white'>
-        <div className="logo text-xl text-center">copytright &copy;Get Me aChai 2025</div>
+        <div className="logo text-xl text-center">copytright &copy;personaX 2025</div>
         
     </div>
       </footer>
