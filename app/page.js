88a9a6dc26e1d88a0d7a1ef8606b9cefab4bde1f
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 13:00:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <div className="h-64 gap-3  flex flex-col justify-center items-center  bg-[url(https://images.pexels.com/photos/1242348/pexels-photo-1242348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)] bg-no-repeat
+      <div className="h-64 gap-3  flex flex-col justify-center items-center  bg-[url(https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)] bg-no-repeat
 bg-cover ">
         <h2 className="text-5xl font-bold ">Persona<span className="text-green-500">&lt;X&gt; </span></h2>
         <p className="font-bold">Connect, Express, Evolve.</p>
