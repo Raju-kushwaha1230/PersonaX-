740a2dba78a48333218d6854d7f74c8519b70fb9
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 13:04:12 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,8 @@ const Navbar = () => {
   // }
   return (
 
-    <div className='flex justify-between items-center relative bg-blue-950  px-8 h-16 text-white'>
+    <div className='flex justify-between items-center relative bg-blue-950  px-8 h-16 text-white bg-[url(https://images.pexels.com/photos/1831234/pexels-photo-1831234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)] bg-no-repeat
+bg-cover '>
       <div className="logo text-2xl font-bold "><h2 className="text-4xl font-bold ">Persona<span className="text-green-500">&lt;X&gt; </span></h2></div>
       <ul>
         <Link href={"/"} className='p-3 font-bold text-xl pr-10'>Home </Link>
