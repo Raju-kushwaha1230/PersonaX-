2214cf75384bea886f39275a5d7101beb6252c24
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-27 00:14:30 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-
+import GoogleProvider from "next-auth/providers/google";
 import User from "@/models/User";
 import connectDB from "@/db/connectDb";
 
@@ -21,10 +21,10 @@ export const authOption = NextAuth({
     //   clientId: process.env.FACEBOOK_ID,
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID,
-    //   clientSecret: process.env.GOOGLE_SECRET
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
     // // Passwordless / email sign in
     // EmailProvider({
     //   server: process.env.MAIL_SERVER,
@@ -33,7 +33,7 @@ export const authOption = NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      if (account.provider == "github") {
+      if (account.provider == "github"  || account.provider == "google") {
         await connectDB();
         //find user in db
         const currentUser = await User.findOne({ email: email });
