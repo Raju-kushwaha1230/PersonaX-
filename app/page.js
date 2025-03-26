f9b5d8cdefe24bb93a5bd93f4062c7b2864f9e97
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 16:58:30 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ bg-cover ">
 
         <div>
           <Link href="/login"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Get Started</button> </Link>
-            <Link href="/login"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Explore a Demo Profile</button> </Link>
+            <Link href="/mighty"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Explore a Demo Profile</button> </Link>
 
         </div>
       </div>
