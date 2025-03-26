30d44b22c0f89b43c8de000ca671e03a3eabaf16
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 19:21:27 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
       <footer>
-      <div className='h-15 flex flex-row justify-center items-center bg-blue-950 px-8  text-white'>
+      <div className='h-16 flex flex-row justify-center items-center bg-blue-950 px-8  text-white'>
         copyright by <h2 className="text-2xl font-bold ">&copy; Persona<span className="text-green-500">&lt;X&gt; </span></h2>
         
     </div>
