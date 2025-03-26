6953a80d8f97667074b3fe1390a48bda35ccb376
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-26 15:21:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -19,27 +19,36 @@ bg-cover ">
 
         </div>
       </div>
-      <div className="bg-slate-500 h-1 opacity-50">
-
-      </div>
+    
       <div className="space-y-10 pb-10">
         <h2 className="text-center text-2xl font-bold">Connect with Us</h2>
         <div className="flex justify-around items-center">
           <div className="flex flex-col justify-center items-center">
             <img width={200} src="mentorship.gif" alt="" />
             <p className="font-bold">Fans want to help</p>
-            <p>Your fans are available to donate</p>
+            <p>Empower others by sharing knowledge and expertise.
+
+</p>
+              <Link href="/"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Helping</button> </Link>
+
           </div>
           <div className="flex flex-col justify-center items-center">
             <img width={200} src="rupee.gif" alt="" />
             <p className="font-bold">Fans want to contribute</p>
-            <p>Your fans are willing to contribute financilaly</p>
+            <p>Showcase your skills and contribute to the community.
+
+</p>
+              <Link href="/"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Contribute Now</button> </Link>
 
           </div>
           <div className="flex flex-col justify-center items-center">
             <img width={200} src="mentoring.gif" alt="" />
             <p className="font-bold">Fans want to collaborate</p>
-            <p>Your fans are ready to collaborate with you</p>
+            <p>Connect, team up, and build something amazing together.
+
+</p>
+                <Link href="/"><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Collaborate Today</button> </Link>
+
           </div>
         </div>
       </div>
