02859e6e089622c569655e8a9f87563f31302be7
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-30 01:02:51 +0530

    Update PostList.js

--- components/PostList.js
+++ components/PostList.js
@@ -173,10 +173,10 @@ export default function PostList() {
             {/* Media (if any) */}
             {post.media.length > 0 && (
               <img
-             width={400}
+             width={450}
                 src={post.media[0].fileUrl}
                 alt="Post Media"
-                className="w-full   object-cover p-3  mt-2 rounded-md shadow-md"
+                className="   object-cover p-3  mt-2 rounded-md shadow-md"
               />
             )}
 
