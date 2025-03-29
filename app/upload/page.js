86e262c0fe15962355df6a06de9aa89a4d47767f
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-29 18:24:01 +0530

    Create page.js

---
+++ app/upload/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import UploadForm from '@/components/UploadForm'
+const page = () => {
+  return (
+    <div>
+        <UploadForm />
+    </div>
+  )
+}
+
+export default page
