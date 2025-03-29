31b7da65f94e91a25d0454081508a7c3a968c5cb
Raju Kushwaha <mightyrajukushwaha@gmail.com>
2025-03-29 18:28:04 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,37 +1,41 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-
 const userSchema = new Schema({
-    name: {
-        type: String,
-        
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    profilePic: {
-        type: String,
-        
-    },
-    CoverPic: {
-        type: String,
-        
-    },
-    company: {
-        type: String,
-        
-    },
-    resetToken: String,
-    expireToken: Date
-    })
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+  },
+  CoverPic: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  bio: { type: String, default: "" },
+  location: { type: String, default: "" },
+  website: { type: String, default: "" },
+  portfolio: { type: String, default: "" },
+  skills: { type: [String], default: [] },
+  joinedDate: { type: Date, default: Date.now },
+    followers: { type: [String], default: [] },
+    following: { type: [String], default: [] },
+    about: { type: String, default: "" },
 
+  resetToken: String,
+  expireToken: Date,
+});
 
-    // export default  mongoose.models.User ||  model('User', userSchema) ;
-    const User = models.User || model("User", userSchema);
+// export default  mongoose.models.User ||  model('User', userSchema) ;
+const User = models.User || model("User", userSchema);
 
 export default User;
